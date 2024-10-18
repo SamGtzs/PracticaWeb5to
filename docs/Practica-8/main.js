d6f5b3cc96f35ea6b7bd5db832818318814cbda1
MasterPedro1 <samuelgtz025@gmail.com>
2024-10-17 18:49:12 -0600

    algo

--- docs/Practica-8/main.js
+++ docs/Practica-8/main.js
@@ -1,42 +1,12 @@
-/*
+
 async function obtenerDatosDelJSON() {
-    try {
-      let res = await fetch("data.json");
-  
-      if (!res.ok) {
-        throw "Error al acceder al archivo JSON";
-      }
-  
-      let json = await res.json();
-  
-      console.log(res, json);
-  
-      const $peliculas = document.querySelector("#peliculas");
-  
-      let html = "";
-  
-      json.peliculas.forEach((el) => {
-        html += `
-          <article>
-            <img src="${el.poster}" alt="${el.nombre}" />
-            <h4>${el.nombre}</h4> 
-            <h5>${el.estreno}</h5> 
-          </article>
-        `;
-      });
-  
-      $peliculas.innerHTML = html;
-    } catch (error) {
-      console.warn(error);
-    }
+    
   }
-    */
-  
+    
   async function obtenerDatosDeAPI() {
     try {
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>console.log(json))
+        let url = ('https://fakestoreapi.com/products')
+         let res = await fetch(url);
   
       if (!res.ok) {
         throw "Error al acceder a la API";
@@ -53,11 +23,10 @@ async function obtenerDatosDelJSON() {
       json.forEach((el) => {
         html += `
           <article>
-            <h5>ID: ${el.id} </h5> 
+            <img src= "${el.image}"
             <h3> ${el.title} </h3>
-            <h3> ${el.category}</h3>
             <h3> ${el.description} </h3>
-            <p> ${el.image} </p> 
+            <h3> ${el.price}</h3>
           </article>
         `;
       });
